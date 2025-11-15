893f9121d9fc2f1c69b549565e853e830bf3e079
Luan Lisboa <lluan4593@gmail.com>
2025-11-15 00:08:47 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/Lisboa/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Lisboa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Lisboa.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+}
